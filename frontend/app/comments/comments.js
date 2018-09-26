ebb9afb3644b22d410167cb4c6d16482db208d95
azertyfun <azertyfun_poubelle@hotmail.fr>
2018-09-26 18:05:52 +0200

    Added vote UI

--- frontend/app/comments/comments.js
+++ frontend/app/comments/comments.js
@@ -25,6 +25,9 @@ angular.module('feedbackApp.comments', ['ngRoute'])
         status: '',
         buttonDisabled: false
       };
+      comment.vote = {
+        status: ''
+      };
     });
   }, (response) => {
     console.error(`Could not fetch /feedback/${id}/comments: ${response}`);
@@ -33,6 +36,7 @@ angular.module('feedbackApp.comments', ['ngRoute'])
 
   $scope.submit = (comment) => {
     comment.reply.buttonDisabled = true;
+    comment.reply.status = 'Uploading...';
     let data = {
       statement: comment.reply.text,
       user: $window.localStorage.getItem('user'),
@@ -42,6 +46,22 @@ angular.module('feedbackApp.comments', ['ngRoute'])
     }, (response) => {
       comment.reply.status = `Error: ${response.data.error === undefined ? 'Unknown error' : response.data.error}`;
       comment.reply.buttonDisabled = false;
+    });
+  };
+
+  $scope.vote = (comment, vote) => {
+    comment.vote.status = 'Voting...';
+
+    let data = {
+      user: $window.localStorage.getItem('user'),
+      vote: vote
+    };
+
+    $http.post(`http://127.0.0.1:8001/feedback/${comment.id}/vote`, data, {}).then((response) => {
+      comment.vote.status = 'Success!';
+    }, (response) => {
+      comment.vote.status = `Error: ${response.data.error === undefined ? 'Unknown error' : response.data.error}`;
+      comment.buttonDisabled = false;
     });
   };
 });
--- frontend/app/home/home.js
+++ frontend/app/home/home.js
@@ -7,11 +7,35 @@ angular.module('feedbackApp.home', ['ngRoute'])
     controller: 'homeController'
   });
 }])
-.controller('homeController', function($scope, $http) {
+.controller('homeController', function($scope, $http, $window) {
+  $scope.loggedIn = window.localStorage.getItem('user') !== undefined;
   $scope.feedback = [];
+
   $http.get('http://127.0.0.1:8001/feedback').then((response) => {
     $scope.feedback = response.data.feedback
+
+    $scope.feedback.forEach(message => {
+      message.vote = {
+        status: ''
+      };
+    });
   }, (response) => {
     console.error(`Could not fetch /feedback: ${response}`);
   });
+
+  $scope.vote = (message, vote) => {
+    message.vote.status = 'Voting...';
+
+    let data = {
+      user: $window.localStorage.getItem('user'),
+      vote: vote
+    };
+
+    $http.post(`http://127.0.0.1:8001/feedback/${message.id}/vote`, data, {}).then((response) => {
+      message.vote.status = 'Success!';
+    }, (response) => {
+      message.vote.status = `Error: ${response.data.error === undefined ? 'Unknown error' : response.data.error}`;
+      message.buttonDisabled = false;
+    });
+  };
 });
